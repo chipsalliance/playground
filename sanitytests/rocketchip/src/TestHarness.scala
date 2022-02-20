9b5d4e66ee5b1d1a77400edc78bbe64599653686
Zenithal <i@zenithal.me>
2022-02-20 23:04:37 +0800

    emulator: enable vcd output

--- sanitytests/rocketchip/src/TestHarness.scala
+++ sanitytests/rocketchip/src/TestHarness.scala
@@ -55,7 +55,8 @@ case class TestHarness[M <: RawModule](
       "+define+RANDOMIZE_GARBAGE_ASSIGN",
       "--output-split 20000",
       "--output-split-cfuncs 20000",
-      "--max-num-width 1048576"
+      "--max-num-width 1048576",
+      "--trace",
       // format: on
     ).mkString(" ")
     val csrcs = Seq("csrc/emulator.cc", "csrc/SimDTM.cc", "csrc/SimJTAG.cc", "csrc/remote_bitbang.cc")
