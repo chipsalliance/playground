e95694313aec808844a7a5e1809695eb587abd36
SingularityKChen <chency_singularity@163.com>
2022-07-05 00:53:19 +0800

    feat: init memory generating function
    + add necessary functions and classes for generating memory using given memory ip;
    + supporting arbitrary bit width;
    + need to fix the function for arbitrary word counts (depth);
    + need to verify the syntax of the generated Verilog files;

--- sanitytests/fusion/src/SynthesisHarness.scala
+++ sanitytests/fusion/src/SynthesisHarness.scala
@@ -53,6 +53,109 @@ case class SynthesisHarness[M <: chisel3.Module](
     logger.warn(s"Scripts location: $scriptsDir")
     scriptsDir
   }
+  def getMemConfig(memBlackBoxFilename: Path): MemCfg = {
+    val regex = raw"// name:(\w+) depth:(\d+) width:(\d+) masked:(\w+) maskGran:(\d+) maskSeg:(\d+)".r
+    val firstLine = os.read.lines(memBlackBoxFilename).head
+    firstLine match {
+      case regex(name, depth, width, masked, maskGran, maskSeg) =>
+        MemCfg(
+          name = name,
+          depth = BigInt(depth),
+          width = width.toInt,
+          masked = masked.toBoolean,
+          maskGran = maskGran.toInt,
+          maskSeg = maskSeg.toInt
+        )
+      case _ =>
+        println("[Error] No regex matched")
+        MemCfg()
+    }
+  }
+  def genSynthMem(memCfg: MemCfg): String = {
+    // all the possible width in the mem compiler. Any Integer in Range(4, 512).
+    val memWidthSeq = Seq(4, 512)
+    val memDepthSeq = Seq(16, 32, 64, 128, 256, 512, 1024)
+    val memName = memCfg.name
+    val memDepth = memCfg.depth
+    val memWidth = memCfg.width
+    val memMasked = memCfg.masked
+    val memMaskGran = memCfg.maskGran
+    val memMaskSeg = memCfg.maskSeg
+    val memAddrWidth = memCfg.addrWidth
+    // get the number of memory banks in case the memory width is greater than 128
+    val memBankWidth = ceil(memWidth.toDouble / memWidthSeq.max.toDouble).toInt
+    // get the number of memory banks in case the depth of the memory is greater than 512
+    val memBankDepth = ceil(memDepth.toDouble / memDepthSeq.max.toDouble).toInt
+    val memBankNumber = memBankWidth * memBankDepth
+    val instBit = if (memBankWidth == 1)
+      max(memWidth, memWidthSeq.min) else
+      memWidth - memWidthSeq.max * (memBankWidth - 1)
+    val instDepth = if (memBankDepth == 1)
+      pow(2, log2Ceil(memDepth)).toInt else
+      max(pow(2, log2Ceil(memDepth - memDepthSeq.max * (memBankDepth - 1))).toInt, memDepthSeq.min)
+    var instIPVerilog = ""
+    for (memBankDepthId <- 0 until memBankDepth) {
+      for (memBankWidthId <- 0 until memBankWidth) {
+        val instId = memBankDepthId * memBankWidth + memBankWidthId
+        val curInstBit = if (memBankWidthId + 1 == memBankWidth) instBit else memWidthSeq.max
+        val curInstDepth = if (memBankDepthId + 1 == memBankDepth) instDepth else memDepthSeq.max
+        val dataStart = memBankWidthId * memWidthSeq.max
+        val dataEnd = dataStart + curInstBit - 1
+        val instName = s"${memName}_inst_$instId"
+        val memIPName = s"SRAM1RW${curInstDepth}x$curInstBit"
+        val inputBus = s"W0_data[$dataEnd:$dataStart]"
+        val outputBus = s"rdata_o_$instId"
+        instIPVerilog += s"    wire [${curInstBit-1}:0] $outputBus;\n"
+        instIPVerilog += genMemInstVerilog(InstCfg(
+          memIPName = memIPName,
+          instName = instName,
+          addrPort = "R0_addr", // FIXME: depth > 1
+          inputBus = inputBus,
+          outputBus = outputBus
+        ))
+      }
+    }
+    instIPVerilog += s"    assign R0_data = {${Range(0, memBankNumber).map(id =>
+      s"rdata_o_$id").reduce((x, y) => s"$y, $x")}};\n"
+    val utilWires =
+      s"""    wire chip_select = R0_en | W0_en;
+         |    wire wen_n = !W0_en;
+         |    wire ren_n = !R0_en;
+         |""".stripMargin
+    val memVerilogString =
+      s"""// name:$memName depth:$memDepth width:$memWidth masked:$memMasked maskGran:$memMaskGran maskSeg:$memMaskSeg
+         |module $memName(
+         |    input R0_clk,
+         |    input [${memAddrWidth-1}:0] R0_addr,
+         |    input R0_en,
+         |    output [${memWidth-1}:0] R0_data,
+         |    input W0_clk,
+         |    input [${memAddrWidth-1}:0] W0_addr,
+         |    input W0_en,
+         |    input [${memWidth-1}:0] W0_data,
+         |    input [${memMaskSeg-1}:0] W0_mask
+         |);
+         |
+         |$utilWires
+         |$instIPVerilog
+         |
+         |endmodule
+         |""".stripMargin
+    memVerilogString
+  }
+  def genMemInstVerilog(instCfg: InstCfg): String = {
+    s"""    ${instCfg.memIPName} ${instCfg.instName} (
+       |        .A  (${instCfg.addrPort}),  // Primary Read/Write Address
+       |        .CE (R0_clk     ),  // Primary Positive-Edge Clock
+       |        .WEN(wen_n      ),  // Primary Write Enable, Active Low
+       |        .OEB(ren_n      ),  // Primary Output Enable, Active Low
+       |        .CSB(chip_select),  // Primary Chip Select, Active Low
+       |        .I  (${instCfg.inputBus}),  // Primary Input data bus
+       |        .O  (${instCfg.outputBus})  // Primary Output data bus
+       |    );
+       |
+       |""".stripMargin
+  }
 }
 
 /** The configuration of the targeted memory*/
