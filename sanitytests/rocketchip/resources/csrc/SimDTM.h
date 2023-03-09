e3e4959ae03056572fe8bf90207c7cd47e01a17d
Jiuyang Liu <liu@jiuyang.me>
2023-03-09 19:58:09 +0800

    remove sanitytests for now

--- sanitytests/rocketchip/resources/csrc/SimDTM.h
+++
@@ -1,9 +0,0 @@
-#include <fesvr/dtm.h>
-
-class ledtm_t : public dtm_t
-{
-  public:
-    ledtm_t(int argc, char**argv);
-    ~ledtm_t();
-    endianness_t get_target_endianness() const;
-};
--- sanitytests/vcu118/src/package.scala
+++
@@ -1,7 +0,0 @@
-package sanitytests
-
-import os._
-
-object utils {
-  def resource(file: String): Path = Path(java.nio.file.Paths.get(getClass().getClassLoader().getResource(file).toURI))
-}
